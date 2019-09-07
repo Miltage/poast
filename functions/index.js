6692c4a510458277a2ce7295448a9d3d16d0d9cd
Robert Shenton <theshenton@gmail.com>
2019-09-07 20:34:54 +0200

    Add cloud function for scoring

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,36 @@
-//const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+
+admin.initializeApp(functions.config().firebase);
+
+let db = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+exports.aggregateRatings = functions.firestore
+  .document("posts/{postId}/votes/{voteId}")
+  .onWrite((change, context) => {
+    // Get value of the newly added rating
+    var voteVal = change.after.data().value;
+
+    // Get a reference to the restaurant
+    var postRef = db.collection("posts").doc(context.params.postId);
+
+    // Update aggregations in a transaction
+    return db.runTransaction(transaction => {
+      return transaction.get(postRef).then(postDoc => {
+        // Compute new score
+        var newScore = postDoc.data().score + voteVal;
+
+        // Update restaurant info
+        return transaction.update(postRef, {
+          score: newScore
+        });
+      });
+    });
+  });
