2e06c2dd5b6db1d4bb1af46e622a40dc5297245f
Robert Shenton <theshenton@gmail.com>
2019-09-07 18:46:09 +0200

    More youtube urls

--- src/shared.js
+++ src/shared.js
@@ -1,6 +1,10 @@
 export default {
   detectContentType: function() {
-    if (this.data.content.includes("youtube.com")) return "youtube";
+    if (
+      this.data.content.includes("youtube.com") ||
+      this.data.content.includes("youtu.be")
+    )
+      return "youtube";
     return "image";
   }
 };
