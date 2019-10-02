9023467d31d759ac473ae50f9aac49e696f2e64f
Robert Shenton <theshenton@gmail.com>
2019-10-02 09:20:22 +0200

    Add info to admin dashboard

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -2,30 +2,75 @@
   <div class="flex text-center justify-around m-10">
     <div class="flex-1 text-xl font-semibold">
       Users
-      <span class="block text-teal-500" style="font-size: 4rem">0</span>
+      <span class="block text-teal-500" style="font-size: 4rem">{{
+        userCount
+      }}</span>
     </div>
     <div class="flex-1 text-xl font-semibold">
       Posts
-      <span class="block text-teal-500" style="font-size: 4rem">0</span>
+      <span class="block text-teal-500" style="font-size: 4rem">{{
+        postCount
+      }}</span>
     </div>
     <div class="flex-1 text-xl font-semibold">
       Posts today
-      <span class="block text-teal-500" style="font-size: 4rem">0</span>
+      <span class="block text-teal-500" style="font-size: 4rem">{{
+        todayCount
+      }}</span>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-//import firebase from "firebase";
+import firebase from "firebase";
+import moment from "moment";
 
 export default {
   name: "dashboard",
   components: {},
   data() {
-    return {};
+    return {
+      userCount: 0,
+      postCount: 0,
+      todayCount: 0
+    };
   },
-  created() {},
-  methods: {}
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      // WARNING: not scalable! Implement counters in the future!
+      // https://stackoverflow.com/questions/46554091/firebase-firestore-collection-count
+
+      firebase
+        .firestore()
+        .collection("users")
+        .get()
+        .then(snap => {
+          this.userCount = snap.size;
+        });
+
+      firebase
+        .firestore()
+        .collection("posts")
+        .get()
+        .then(snap => {
+          this.postCount = snap.size;
+        });
+
+      var today = moment().startOf("day");
+
+      firebase
+        .firestore()
+        .collection("posts")
+        .where("created", ">=", today.toDate())
+        .get()
+        .then(snap => {
+          this.todayCount = snap.size;
+        });
+    }
+  }
 };
 </script>
